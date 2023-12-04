27667603258cbcfd34c9a5f00c29f61ff063b5e3
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2023-12-04 19:21:08 -0300

    more idiomatic code, fix clippy issues, accept multiple messages at once, fix not received message size yet.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
 
-use tokio::{io::AsyncWriteExt, net::TcpStream, time};
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+use tokio::time;
 
 // `u32` (32-bit unsigned int) is used for storing a length of a message.
 // Size of `u32`: 4 bytes.
 // https://doc.rust-lang.org/std/mem/fn.size_of.html
-const MESSAGE_LENGTH_BUFFER_SIZE: usize = 4;
+const MESSAGE_LENGTH_BYTES: usize = 4;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -16,7 +18,7 @@ async fn main() -> io::Result<()> {
     println!("socket is writable");
 
     send_message("hello", &mut socket).await?;
-    sleep(3).await;
+    // no sleep here!! this will put messages on server's buffer all at once.
     send_message("world", &mut socket).await?;
 
     socket.shutdown().await?;
@@ -25,12 +27,11 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
-async fn send_message(message: &str, socket: &mut TcpStream) -> io::Result<()> {
-    println!("[message: {}] starting", message);
+async fn send_message(msg: &str, socket: &mut TcpStream) -> io::Result<()> {
+    println!("[message: {msg}] starting");
 
-    let message_bytes = message.as_bytes();
-
-    let message_len = message_bytes.len() as u32; // cast to u32 is critical to get an array of exactly 4 bytes
+    let message_bytes = msg.as_bytes();
+    let message_len = msg.len() as u32; // cast to u32 is critical to get an array of exactly 4 bytes
 
     // Getting the memory representation of the message length (u32) as a byte array in big-endian (network) byte order
     let message_len_bytes = message_len.to_be_bytes();
@@ -45,17 +46,20 @@ async fn send_message(message: &str, socket: &mut TcpStream) -> io::Result<()> {
     // This is just for illustration of how network can partition data.
     // In general, you'd do:
     // socket.write_all(&bytes).await?;
-    let (head, tail) = bytes.split_at(MESSAGE_LENGTH_BUFFER_SIZE + 2);
-
-    socket.write(&head).await?;
-    println!("[message: {}] head was written to the socket", message);
+    let (head, tail) = bytes.split_at(MESSAGE_LENGTH_BYTES - 2);
+    socket.write_all(head).await?;
+    println!("[message: {msg}] incomplete head was written to the socket");
 
     sleep(1).await;
 
-    socket.write(&tail).await?;
-    println!("[message: {}] tail was written to the socket", message);
+    let (head, tail) = tail.split_at(MESSAGE_LENGTH_BYTES);
+    socket.write_all(head).await?;
+    println!("[message: {msg}] head + half tail was written to the socket");
 
-    println!("[message: {}] sent", message);
+    socket.write_all(tail).await?;
+    println!("[message: {msg}] tail was written to the socket");
+
+    println!("[message: {msg}] sent");
 
     Ok(())
 }
